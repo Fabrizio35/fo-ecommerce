56b5c209d95925926c20cc2502f213df874bd77b
Fabrizio <ossolafabrizio@gmail.com>
2025-08-11 19:46:35 -0300

    change style CartButton

--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCartIcon } from '@/icons'
+import { ShoppingCartFilledIcon, ShoppingCartIcon } from '@/icons'
 import { useCartStore } from '@/store/cartStore'
 
 export default function CartButton() {
@@ -11,7 +11,11 @@ export default function CartButton() {
       className="cursor-pointer flex items-center gap-1 px-2 p-0.5 text-neutral-900 font-bold text-lg hover:text-blue-500 transition-colors duration-300"
     >
       <span>Carrito</span>
-      <ShoppingCartIcon className="size-6" />
+      {cart.length > 0 ? (
+        <ShoppingCartFilledIcon className="size-6 text-blue-500" />
+      ) : (
+        <ShoppingCartIcon className="size-6" />
+      )}
       {cart.length > 0 && (
         <span className="px-1 font-extrabold">{cart.length}</span>
       )}
